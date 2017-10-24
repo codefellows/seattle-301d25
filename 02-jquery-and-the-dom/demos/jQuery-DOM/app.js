717ef565b05b4b8db2f2cf83bc9958fd6267fb72
Allie <allie@codefellows.com>
2017-10-24 14:59:07 -0700

    adding demo for jQuery

---
+++ 02-jquery-and-the-dom/demos/jQuery-DOM/app.js
@@ -0,0 +1,65 @@
+//*****basic selectors*****//
+
+//element
+  // vanillaJS
+
+  // jQuery
+
+//class
+  // vanillaJS
+
+  // jQuery
+
+
+//id
+  // vanillaJS
+
+  // jQuery
+
+
+//parent descendent
+
+
+// parent > child
+
+
+//attribute
+
+
+//get text of the matched element(s)
+
+
+//set text of the matched element(s)
+
+
+//get the html of the matched element(s)
+
+
+//set the html of the matched element(s)
+
+
+//get the data-category attribute of an li
+
+
+//set the data-category attribute of an li
+
+
+//make a new <li> and append it to the <ul>
+  // vanillaJS
+
+  // jQuery
+
+
+//make a clone
+
+
+//remove an element from the DOM, returns the removed element
+
+
+//empty out the contents of the element
+
+
+//run a command as soon as the DOM loads
+
+
+//shorthand
