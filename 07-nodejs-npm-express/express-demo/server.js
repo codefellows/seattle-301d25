5169f6b40cecc1dd78dcfdd0dd40314565007048
Allie <allie@codefellows.com>
2017-10-31 12:54:38 -0700

    pushing demo before class 07

---
+++ 07-nodejs-npm-express/express-demo/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Load the express library from node_modules
+
+// Instantiate express so we can use its functionality
+
+// Designate a port to serve our app on
+// "process" is in the Node environment, use a port if it is set up, or set your own
+
+// Tell the server which directory to serve files from
+
+// Set up a route to send a message
+
+// Set up a route to send a file
+
+// Can we perform other methods on the same route?
+
+// Start the app so it listens for changes
